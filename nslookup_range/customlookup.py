17880d821765a3615439364c5c0c0b548ab59c57
DColePetty <32550153+DColePetty@users.noreply.github.com>
2021-03-04 23:22:08 -0700

    add todo's, python 3.6, and init commit of code
    add todo's, python 3.6, and init commit of code, anonymized default address

--- nslookup_range/customlookup.py
+++ nslookup_range/customlookup.py
@@ -1,14 +1,17 @@
 import os
 import subprocess
 
-VERBOSE = False
-
-net = "129.82.250."
+# PYTHON 3.6 +
+# first three octets plus a dot
+net = "192.168.0."
 hosts = []
+# this range here will be the range for the second octet, currently 1-255 (ik)
 for i in range(1, 255):
     hosts.append(i)
-print(str(hosts))
+print(str(hosts)) # prints the fourth octet values to be tried
 
+# todo tranform this this to a method that can be multithreaded
+# todo add some pretty default output for resolved and unresolved hosts. 
 for i in hosts:
     out1 = subprocess.call(["nslookup", str(net) + str(i)])
     if out1 != 0:
